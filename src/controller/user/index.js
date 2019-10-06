d548b465d387789a8e9312318513f3d44b481097
A. Noval Adip <a.novaladip@yahoo.com>
2019-10-07 01:57:55 +0700

    add kasub

--- src/controller/user/index.js
+++ src/controller/user/index.js
@@ -50,7 +50,7 @@ async function login(req, res) {
     });
   } catch (error) {
     console.log(error);
-    res.json({ message: "Failed login", error });
+    res.status(400).json({ message: "Failed login", error });
   }
 }
 
